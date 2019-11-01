37d3b1a237b6b2eed74c70570def800fb5e87092
unknown <bohanzhang@my.unt.edu>
2019-11-01 13:39:21 -0400

    random_questions

--- inc/generate_questions.php
+++ inc/generate_questions.php
@@ -11,3 +11,20 @@
 // Make sure it is a unique answer
 
 // Add question and answer to questions array
+
+//create a question array and push the generated question into the array
+$questions = [];
+$rangeLow = 1;
+$rangeHigh = 1000;
+$incorrectNumberLow = 1;
+$incorrectNumberHigh = 10;
+
+for($i=0;i<=20;++$i){
+    $leftAdder = random_int($rangeLow, $rangeHigh);
+    $rightAdder = random_int($rangeLow, $rangeHigh);
+    $correctAnswer = $leftAdder + $rightAdder;
+    $incorrectAnswerFirst = random_int($incorrectNumberLow, $incorrectNumberHigh);
+    $incorrectAnswerSecond = random_int($incorrectAnswerFirst, $incorrectNumberHigh);
+
+   
+}
