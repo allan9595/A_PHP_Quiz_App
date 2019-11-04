b194d603b03733eeb45c11a13d8cafb72df19827
*allan9595* <bohanzhang@my.unt.edu>
2019-11-04 14:25:57 -0500

    finalized and finished

--- inc/generate_questions.php
+++ inc/generate_questions.php
@@ -4,14 +4,26 @@
 function generateQuestion(){
     $rangeLow = 1;
     $rangeHigh = 100;
-    $incorrectNumberLow = 1;
     $incorrectNumberHigh = 10;
 
     $leftAdder = random_int($rangeLow, $rangeHigh);
     $rightAdder = random_int($rangeLow, $rangeHigh);
     $correctAnswer = $leftAdder + $rightAdder;
-    $incorrectAnswerFirst = $correctAnswer + random_int($incorrectNumberLow, $incorrectNumberHigh);
-    $incorrectAnswerSecond = $correctAnswer - random_int($incorrectNumberLow , $incorrectNumberHigh);
+    //generate init values for envaluation
+    $incorrectAnswerFirst = random_int($correctAnswer-$incorrectNumberHigh, $correctAnswer+$incorrectNumberHigh);
+    $incorrectAnswerSecond = random_int($correctAnswer-$incorrectNumberHigh , $correctAnswer+$incorrectNumberHigh);
+
+    //if the condition not met, keep generated
+    while(
+        ($incorrectAnswerFirst == $correctAnswer) 
+        ||  
+        ($incorrectAnswerFirst == $incorrectAnswerSecond)
+        ||
+        ($incorrectAnswerSecond == $correctAnswer)
+    ){
+        $incorrectAnswerFirst = random_int($correctAnswer-$incorrectNumberHigh, $correctAnswer+$incorrectNumberHigh);
+        $incorrectAnswerSecond = random_int($correctAnswer-$incorrectNumberHigh , $correctAnswer+$incorrectNumberHigh);
+    }
     $questions =
         [
             "leftAdder" => $leftAdder,
